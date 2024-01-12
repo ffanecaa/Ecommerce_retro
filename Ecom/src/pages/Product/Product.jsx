dcfddb5f102d77088900cd311d7e9163b8a2a8cd
marta <105271643+ffanecaa@users.noreply.github.com>
2024-01-12 13:31:05 +0100

    m

--- Ecom/src/pages/Product/Product.jsx
+++ Ecom/src/pages/Product/Product.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
 import "./Product.css"
 const Product = () => {
+
+const dataImages =[
+  "https://d2j6dbq0eux0bg.cloudfront.net/images/93050517/3905839804.jpg",
+  "https://d2j6dbq0eux0bg.cloudfront.net/images/93050517/3899716069.jpg",
+]
+
+const[selectImage,SetSelectImage]= useState(1)
+const handleimage0 = ()=>{
+  SetSelectImage(0)
+}
+const handleimage1 = ()=>{
+  SetSelectImage(1)
+}
   return (
-    <div className='Product'>Product</div>
+    <div className='product'>
+<div className="product_left">
+   <div className="productImage">
+    <img className="imgprod" src={dataImages[0]}alt=""  onClick={handleimage0}/>
+    <img className="imgprod"src={dataImages[1]} alt=""  onClick={handleimage1}/>
+   </div>
+   <div className="mainproductImage">
+    <img  className="mainImg"src={dataImages[selectImage]} alt="" />
+   </div>
+</div>
+
+<div className="product_right"></div>
+
+
+
+
+    </div>
   )
 }
 
--- Ecom/src/pages/Products/Products.jsx
+++ Ecom/src/pages/Products/Products.jsx
@@ -1,9 +1,80 @@
-import  './Products.css'
-
+import List from "../../componets/List/List";
+import "./Products.css";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 const Products = () => {
-  return (
-    <div className='Products'>Products</div>
-  )
-}
+  const idCategorie = parseInt(useParams().id);
+  console.log(idCategorie);
+  const [maxPrice, setMaxPrice] = useState(1000);
 
-export default Products
+  const HandlePriceMax = (e) => {
+    setMaxPrice(e.target.value);
+  };
+  const [sort, setSort] = useState(null);
+  const handleSortChange = (e) => {
+    setSort(e.target.value);
+  };
+  console.log(sort);
+  return (
+    <div className="products">
+      <div className="products_left">
+        <div className="filterProduct">
+          <h2 className="h2filter">categories</h2>
+          <div className="inputItemProduct">
+            <input type="checkbox" name="" id="1" />
+            <label htmlFor="1">consolas</label>
+          </div>
+          <div className="inputItemProduct">
+            <input type="checkbox" name="" id="2" />
+            <label htmlFor="2">televisores</label>
+          </div>
+          <div className="inputItemProduct">
+            <input type="checkbox" name="" id="3" />
+            <label htmlFor="3">ordenadores</label>
+          </div>
+        </div>
+        <div className="filterProduct">
+          <h2 className="h2filter">prices</h2>
+          <div className="inputItemProduct">
+            <span>0</span>
+            <input type="range" min={0} max={1000} onChange={HandlePriceMax} />
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+        <div className="filterProduct">
+          <h2 className="h2filter">sort</h2>
+          <div className="inputItemProduct">
+            <input
+              type="radio"
+              name="price"
+              value="asc"
+              id="asc"
+              checked={sort === "asc"}
+              onChange={handleSortChange}
+            />
+            <label htmlFor="asc">precio mas bajo</label>
+          </div>
+          <div className="inputItemProduct">
+            <input
+              type="radio"
+              name="price"
+              value="desc"
+              id="desc"
+              checked={sort === "desc"}
+              onChange={handleSortChange}
+            />
+            <label htmlFor="desc">precio mas alto primero</label>
+          </div>
+        </div>
+        </div>
+        <div className="products_right">
+          <img src="/habita.jpg" alt="" />
+          {/* fotoss */}
+          <List idCategorie={idCategorie} maxPrice={maxPrice} sort={sort} />
+        </div>
+     
+    </div>
+  );
+};
+
+export default Products;
