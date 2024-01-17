481b245eb0b24331f90f4762537c0b45f7e5ef70
marta <105271643+ffanecaa@users.noreply.github.com>
2024-01-17 18:42:57 +0100

    v

--- Ecom/src/pages/categories/Categories.jsx
+++ Ecom/src/pages/categories/Categories.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import useProducts from "../../Hook/useProducts";
 import Cards from "../../componets/cards/Cards";
 
